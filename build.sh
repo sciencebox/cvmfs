f5edfedb41a04952050e29ebc703c13dd8bde491
Enrico Bocchi <enrico.bocchi@cern.ch>
2021-01-04 19:07:14 +0100

    Helper script to build and push at desired version

---
+++ build.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Tag name
+BASE_TAG='gitlab-registry.cern.ch/sciencebox/docker-images/cvmfs'
+
+# Specify cvmfs version to be installed (or comment out to use the latest)
+CVMFS_VERSION='2.7.5'
+
+# Build the Docker image
+if [ -z $CVMFS_VERSION ]; then
+  TAG="$BASE_TAG:latest"
+  docker build -t $TAG .
+else
+  TAG="$BASE_TAG:$CVMFS_VERSION"
+  docker build --build-arg CVMFS_VERSION=-$CVMFS_VERSION -t $TAG .
+fi
+
+# Push the image to the GitLab registry
+docker login gitlab-registry.cern.ch
+docker push $TAG
+
